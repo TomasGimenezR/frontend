c86068825c7f9d04d49bc9bdafe3decc97b7de10
TomasGimenezR <tomasgimenezr@gmail.com>
2019-09-26 15:13:40 -0300

    Primer commit

--- app/src/main/java/com/project/alertavecinal/MainActivity.java
+++ app/src/main/java/com/project/alertavecinal/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.onesignal.OneSignal;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
         setContentView(R.layout.activity_main);
     }
 }
